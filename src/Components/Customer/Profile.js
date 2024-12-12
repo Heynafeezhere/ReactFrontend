bb02dd1d73c8c64ec3d636eab9d5c1fcbc073303
heynafeezhere <nafeezv11@gmail.com>
2024-12-12 08:28:10 +0530

    Update Profile

--- src/Components/Customer/Profile.js
+++ src/Components/Customer/Profile.js
@@ -1,44 +1,192 @@
+import { useEffect, useState } from 'react';
 import Sidebar from './Sidebar';
+import UserContext from '../../Context';
+import { useContext } from 'react';
+import axios from 'axios';
 
 function Profile() {
+    const [profileData, setProfileData] = useState({});
+    const [updatedProfileData, setUpdatedProfileData] = useState({});
+    const userContext = useContext(UserContext);
+    const baseUrl = "http://127.0.0.1:8000/api/";
+
+    useEffect(() => {
+        // Fetch user data based on customerId from context
+        if (userContext.customerId) {
+            fetch(`${baseUrl}customer/${userContext.customerId}/`)
+                .then((response) => response.json())
+                .then((data) => {
+                    setProfileData(data);
+                    setUpdatedProfileData(data);
+                });
+        }        
+    }, [userContext.customerId]); // Add dependency to re-fetch when customerId changes
+
+    function updateProfileHandler(event) {
+        event.preventDefault(); // Prevent default form submission behavior
+        
+        const updatedUser = new FormData();
+        updatedUser.append('first_name', updatedProfileData.user.first_name);
+        updatedUser.append('last_name', updatedProfileData.user.last_name);
+        updatedUser.append('username', updatedProfileData.user.username);
+        updatedUser.append('email', updatedProfileData.user.email);
+
+        axios.put(`${baseUrl}user/${userContext.customerId}/`, updatedUser, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+            .then((response) => {
+                console.log('Customer profile updated successfully:', response.data);
+            })
+            .catch((error) => {
+                console.error('Error updating customer profile:', error);
+            });
+
+        const updatedData = new FormData();
+        updatedData.append('phone', updatedProfileData.phone);
+        if (updatedProfileData.profile_image) {
+            updatedData.append('profile_image', updatedProfileData.profile_image);
+        }
+
+        axios.put(`${baseUrl}customer/${userContext.customerId}/`, updatedData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+            .then((response) => {
+                console.log('Customer profile updated successfully:', response.data);
+            })
+            .catch((error) => {
+                console.error('Error updating customer profile:', error);
+            });
+        
+    };
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        setUpdatedProfileData((prevData) => ({ ...prevData, profile_image: file }));
+    };
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setUpdatedProfileData((prevData) => {
+            if (name === 'firstName') {
+                return { ...prevData, user: { ...prevData.user, first_name: value } };
+            } else if (name === 'lastName') {
+                return { ...prevData, user: { ...prevData.user, last_name: value } };
+            } else if (name === 'userName') {
+                return { ...prevData, user: { ...prevData.user, username: value } };
+            } else if (name === 'email') {
+                return { ...prevData, user: { ...prevData.user, email: value } };
+            } else if (name === 'phone') {
+                return { ...prevData, phone: value };
+            } else {
+                return prevData;
+            }
+        });
+        console.log(updatedProfileData);
+    };
+
     return (
         <section className="container mt-4">
             <div className="row mt-3">
-                <div className='col-md-3 col-12 mb-2'>
+                <div className="col-md-3 col-12 mb-2">
                     <Sidebar />
                 </div>
-                <div className='col-md-9 col-12 mb-2'>
-                    <div className='card'>
-                        <div className='card-header'>
+                <div className="col-md-9 col-12 mb-2">
+                    <div className="card">
+                        <div className="card-header">
                             <h4>Update Customer Profile</h4>
                         </div>
-                        <div className='card-body'>
-                            <form>
+                        <div className="card-body">
+                            <form onSubmit={updateProfileHandler}>
                                 <div className="mb-3">
-                                    <label for="firstName" className="form-label"><h5>First Name</h5></label>
-                                    <input type="text" className="form-control" id="firstName" />
+                                    <label htmlFor="firstName" className="form-label">
+                                        <h5>First Name</h5>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        value={updatedProfileData?.user?.first_name || ''}
+                                        onChange={handleInputChange}
+                                        name="firstName"
+                                        className="form-control"
+                                        id="firstName"
+                                    />
                                 </div>
                                 <div className="mb-3">
-                                    <label for="lastName" className="form-label"><h5>Last Name</h5></label>
-                                    <input type="text" className="form-control" id="lastName" />
+                                    <label htmlFor="lastName" className="form-label">
+                                        <h5>Last Name</h5>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        value={profileData?.user?.last_name || ''}
+                                        onChange={handleInputChange}
+                                        name="lastName"
+                                        className="form-control"
+                                        id="lastName"
+                                    />
                                 </div>
                                 <div className="mb-3">
-                                    <label for="userName" className="form-label"><h5>Username</h5></label>
-                                    <input type="text" className="form-control" id="userName" />
+                                    <label htmlFor="userName" className="form-label">
+                                        <h5>Username</h5>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        value={profileData?.user?.username || ''}
+                                        onChange={handleInputChange}
+                                        name="userName"
+                                        className="form-control"
+                                        id="userName"
+                                    />
                                 </div>
                                 <div className="mb-3">
-                                    <label for="email" className="form-label"><h5>Email</h5></label>
-                                    <input type="email" className="form-control" id="email" />
+                                    <label htmlFor="email" className="form-label">
+                                        <h5>Email</h5>
+                                    </label>
+                                    <input
+                                        type="email"
+                                        value={profileData?.user?.email || ''}
+                                        onChange={handleInputChange}
+                                        name="email"
+                                        className="form-control"
+                                        id="email"
+                                    />
                                 </div>
                                 <div className="mb-3">
-                                    <label for="profilePicture" className="form-label">
+                                    <label htmlFor="phone" className="form-label">
+                                        <h5>Phone</h5>
+                                    </label>
+                                    <input
+                                        type="phone"
+                                        value={profileData?.phone || ''}
+                                        onChange={handleInputChange}
+                                        name="phone"
+                                        className="form-control"
+                                        id="phone"
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="profile_image" className="form-label">
+                                        <p>
+                                            <img
+                                                src={profileData?.profile_image || 'https://via.placeholder.com/150'}
+                                                alt="Profile Image"
+                                                style={{ width: '150px', height: '150px' }}
+                                            />
+                                        </p>
                                         <h5>Upload Profile Picture</h5>
                                     </label>
-                                    <input type="file" className="form-control" id="profilePicture" accept="image/*" />
+                                    <input type="file" onChange={handleFileChange} name="profile_image" className="form-control" id="profile_image" accept="image/*" />
                                 </div>
                                 <div className="mt-5">
-                                    <button type="submit" className="btn btn-primary">Submit</button>
-                                    <button type="reset" className="btn btn-danger ms-2">Reset</button>
+                                    <button type="submit"
+                                        disabled={!updatedProfileData?.user?.first_name || !updatedProfileData?.user?.last_name || !updatedProfileData?.user?.username || !updatedProfileData?.user?.email || !updatedProfileData?.phone}
+                                        className="btn btn-primary"
+                                        onClick={updateProfileHandler}
+                                    >
+                                        Submit
+                                    </button>
                                 </div>
                             </form>
                         </div>
@@ -46,7 +194,7 @@ function Profile() {
                 </div>
             </div>
         </section>
-    )
+    );
 }
 
 export default Profile;
